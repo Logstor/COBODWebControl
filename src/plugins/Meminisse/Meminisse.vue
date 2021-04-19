27729c757887cf392eb78df9fb39d042bd71d01a
Logstor <a.rottger_rydahl@live.dk>
2021-04-19 19:57:17 +0200

    DWC cleaned for new approach

--- src/plugins/Meminisse/Meminisse.vue
+++ src/plugins/Meminisse/Meminisse.vue
@@ -67,11 +67,7 @@ h1 {
 			</h1>
 			<p>
 				This plugin will log all the movement of the machine to grasp the wear of the components.<br>
-				Logging is currently {{this.loggingState}}.
 			</p>
-			<div id="btndiv">
-				<v-btn @click="onClick" :elevation="this.elevation">Try GET Request</v-btn>
-			</div>
 		</div>
 		
 		<div id="bottomdiv">
@@ -103,21 +99,15 @@ h1 {
 <script>
 'use strict'
 
-import { mapState } from 'vuex';
-import { isPrinting, isPaused } from '../../store/machine/modelEnums.js';
-import { Log } from './log';
-import { sleep } from './index.js';
-import BaseConnector from '../../store/machine/connector/BaseConnector.js'
+//import { Log } from './log';
+//import { sleep } from './index.js';
+import PollConnector from '../../store/machine/connector/PollConnector.js'
 
 export default {
 	data() {
 		return {
 			elevation: "2",
-			active: true,
-
-			logDelay: 2000,
-
-			logList: [new Log("Filename", Date.now(), ["Hek"])],
+			logList: [],
 		}
 	},
 
@@ -125,16 +115,17 @@ export default {
 	 * Private properties
 	 */
 	created: {
-		localStorageLogName: "currentLog"
+		
 	},
 	
 	computed: {
-		...mapState('machine/model', ['job', 'move', 'state']),
-		isJobRunning: state => isPrinting(state.state.status),
-		isJobPaused: state => isPaused(state.state.status),
-		loggingState() {
-			return this.active ? "On" : "Off";
-		},
+		
+	},
+
+	asyncComputed: {
+		async fileList() {
+			
+		}
 	},
 
 	props: {
@@ -152,133 +143,26 @@ export default {
 
 		onClick() {
 			this.printd("Button clicked");
-
-			let prom = BaseConnector.request("GET", "/machine/COBOD/Log");
-			this.printd(prom);
 		},
 
-		/**
-		 * Returns an array of visible axis as classes.
-		 */
-		visibleAxis() { return this.move.axes.filter(axis => axis.visible); },
+		async getFileList() {
+			this.printd("Trying to connect and get file list")
+			let connection = PollConnector.connect();
+			let folders = await connection.getFileList("0:/Meminisse/data/2021");
+			this.printd("Done getting filelist")
 
-		/**
-		 * Adds a downloadable log to the list over downloadable logs.
-		 * 
-		 * @param {Log} log The class instance of Log to add.
-		 */
-		addLog(log) {
-			this.printd("addLog()");
-			this.logList.push(log);
+			this.printd(folders);
+
+			return folders;
 		},
-
-		/**
-		 * Saves an instance of the log to localstorage for later use.
-		 * 
-		 * @param {Log} log The class instance of Log to save.
-		 */
-		addLogLocalStorage(log) {
-			localStorage.setItem(this.localStorageLogName, JSON.stringify(log));
-		},
-
-		clearLogLocalStorage() {
-			localStorage.removeItem(this.localStorageLogName);
-		},
-
-		retrieveLogFromLocalStorage() {
-			// Try to retrieve the old log
-			let log = JSON.parse(localStorage.getItem(this.localStorageLogName));
-
-			// Check there was any
-			if (log == null)
-			{
-				if (this.debug)
-					this.printd("There wasn't any old log in the localstorage");
-				return null;
-			}
-
-			// Make sure it's parsed correctly
-			if (typeof log != 'object')
-				throw "Retrieved log isn't of type Class Log!";
-
-			return log;
-		},
-
-		async log() {
-			this.printd("async log()");
-
-			// Wait for print to start
-			this.printd("Waiting for job starting");
-			while(!this.isJobRunning) await sleep(1000);
-
-			let axis; let currLog;
-			let oldLog = this.retrieveLogFromLocalStorage();
-			if (oldLog == null) 
-			{
-				this.printd("Didn't find old log");
-				this.printd(`Starting print log with filename ${this.job.file.fileName}`);
-
-				// Make log header depending on axis.
-				axis = this.visibleAxis();
-				let headers = new Array();
-				for (let i=0; i < axis.length; ++i)
-					headers.push(axis[i].letter);
-				currLog = new Log(this.job.file.fileName, Date.now, headers);
-			}
-			else
-			{
-				this.printd(`Found old log with filename ${this.job.file.fileName}`);
-				axis.visibleAxis();
-				currLog = oldLog;
-			}
-
-			// Make sure we don't lose the current log on refresh
-			window.onbeforeunload = function() {
-				this.addLogLocalStorage(currLog);
-			};
-
-			// Continously add to log
-			this.printd("Going into log loop");
-			while (this.active && this.isJobRunning)
-			{
-				// Delay
-				await sleep(this.logDelay);
-
-				// Make sure the print isn't paused
-				if (this.isJobPaused)
-					continue;
-				
-				// Record every position and add to log
-				let positions = new Array();
-				for (let i=0; i < axis.length; ++i)
-				{
-					let pos = axis[i].machinePosition;
-					positions.push( (pos != null) ? pos : -1 );
-				}
-				currLog.addEntry(positions);
-			}
-			this.printd("Out of log loop");
-
-			// Done with current print, then make log available for download
-			this.addLog(currLog);
-			this.clearLogLocalStorage();
-			window.onbeforeunload = null;
-
-			// Prepare for logging again
-			if (this.active)
-				this.log();
-		}
 	},
 
 	/**
 	 * This function is fired, when the component is loaded. It only runs when the plugin gets started.
 	 */
 	mounted: function() {
-		if (this.debug)
-			this.printd("Meminisse is mounted!");
-
-		// Start the asynchronious logging
-		this.log();
+		this.printd("Meminisse is mounted!");
+		this.printd(this.getFileList());
 	}
 }
 
