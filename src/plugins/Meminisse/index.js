7a91a1216ebf8fb4a1bdc258f38810189f3c553c
Logstor <a.rottger_rydahl@live.dk>
2021-04-20 15:24:26 +0200

    Create File and Directory classes

--- src/plugins/Meminisse/index.js
+++ src/plugins/Meminisse/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { registerRoute } from '../../routes'
+import Path from '../../utils/path.js'
 
 import Meminisse from './Meminisse.vue'
 
@@ -20,4 +21,37 @@ registerRoute(Meminisse, {
 export function sleep(msec)
 {
 	return new Promise(resolve => setTimeout(resolve, msec));
+}
+
+export class File
+{
+	constructor(path, isDirectory, size, lastModified)
+	{
+		this.path = path;
+		this.isDirectory = isDirectory;
+		this.size = size;
+		this.lastModified = lastModified;
+	}
+
+	getName() { return Path.extractFileName(this.path); }
+}
+
+export class Directory extends File
+{
+	constructor(path, lastModified)
+	{
+		super(path, true, 0, lastModified);
+		this.subDirectories = [];
+		this.files = [];
+	}
+
+	addSubDirectory(directory)
+	{
+		if (directory instanceof Directory) this.subDirectories.push(directory);
+	}
+
+	addFile(file)
+	{
+		if (file instanceof File) this.files.push(file);
+	}
 }
